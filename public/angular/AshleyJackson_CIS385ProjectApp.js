c9f1893a64e2b99dd855e951a6075c5883ef6e73
aajacks9 <aajacks@gmail.com>
2018-04-16 08:20:55 -0500

    created angular controller

--- public/angular/AshleyJackson_CIS385ProjectApp.js
+++ public/angular/AshleyJackson_CIS385ProjectApp.js
@@ -1,1 +1,33 @@
 angular.module('AshleyJackson_CIS385ProjectApp',[]);
+
+var itemListCtrl = function ($scope) {
+    $scope.data = {
+        items: [{
+            name: 'Nemo',
+            rating: 3,
+            genre: 'Childrens'
+            _id:'5370a35f2536f6785f8dfb6a'
+        },{
+            name: 'Space Jam',
+            rating: 5,
+            genre: 'Cartoon'
+            _id:'5370a35f2536f6785f8dfb6a'
+
+        },{
+            name:'Get Out',
+            rating: 5,
+            genre: 'Thriller'
+            _id:'5370a35f2536f6785f8dfb6a'
+
+        },{
+            name:'Meet the Fockers',
+            rating:4,
+            genre:'Comedy'
+            _id:'5370a35f2536f6785f8dfb6a'
+        }]
+    };
+};
+
+angular
+    .module('AshleyJackson_CIS385ProjectApp')
+    .controller('itemsListCtrl',itemsListCtrl);
